bc8ce7cebfb3e31550eaeebfc3aaa00d83ff2e89
Naman <89750281+nagfx@users.noreply.github.com>
2023-02-02 22:29:00 +0500

    Add files via upload

---
+++ module_5/assignments/code_along_1/submission/src/components/AppContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <ProductForm @message="receiveEmitAgain" />
+</template>
+
+<script>
+import ProductForm from "@/components/ProductForm.vue";
+export default {
+  components: { ProductForm },
+  methods: {
+    receiveEmitAgain(value) {
+      console.log(value);
+    },
+  },
+};
+</script>
---
+++ module_5/assignments/code_along_1/submission/src/components/ProductInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <input type="text" v-model="message" /> <br />
+  <button @click="emitData">Emit data</button>
+</template>
+
+<script>
+export default {
+  emits: ["message"],
+  data() {
+    return {
+      message: "hello",
+    };
+  },
+  methods: {
+    emitData() {
+      let message = this.message;
+      this.$emit("message", message);
+    },
+  },
+};
+</script>
